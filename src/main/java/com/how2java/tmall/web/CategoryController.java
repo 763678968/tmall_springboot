8504993164fbb8238271d915c6c01b074e216175
animation <763678968@qq.com>
2019-12-02 14:16:31 +0800

    后台-分类管理-分页 修改CategoryController的list方法

--- src/main/java/com/how2java/tmall/web/CategoryController.java
+++ src/main/java/com/how2java/tmall/web/CategoryController.java
@@ -2,8 +2,10 @@ package com.how2java.tmall.web;
 
 import com.how2java.tmall.pojo.Category;
 import com.how2java.tmall.service.CategoryService;
+import com.how2java.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,7 +17,9 @@ public class CategoryController {
     CategoryService categoryService;
 
     @GetMapping("/categories")
-    public List<Category> list() throws Exception {
-        return categoryService.list();
+    public Page4Navigator<Category> list(@RequestParam(value = "start", defaultValue = "0") int start, @RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
+        start = start<0?0:start;
+        Page4Navigator<Category> page = categoryService.list(start, size, 5); //5表示导航分页最多有5个，像 [1,2,3,4,5] 这样
+        return page;
     }
 }

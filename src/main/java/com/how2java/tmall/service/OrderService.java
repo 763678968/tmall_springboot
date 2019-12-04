fd6013a28feee524363eb4bdaa7eab8829eb6546
animation <763678968@qq.com>
2019-12-04 18:47:51 +0800

    前台-需要登陆-生成订单

--- src/main/java/com/how2java/tmall/service/OrderService.java
+++ src/main/java/com/how2java/tmall/service/OrderService.java
@@ -10,12 +10,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
 public class OrderService {
-
     public static final String waitPay = "waitPay";
     public static final String waitDelivery = "waitDelivery";
     public static final String waitConfirm = "waitConfirm";
@@ -23,14 +23,14 @@ public class OrderService {
     public static final String finish = "finish";
     public static final String delete = "delete";
 
-    @Autowired
-    OrderDAO orderDAO;
+    @Autowired OrderDAO orderDAO;
+    @Autowired OrderItemService orderItemService;
 
     public Page4Navigator<Order> list(int start, int size, int navigatePages) {
         Sort sort = new Sort(Sort.Direction.DESC, "id");
-        Pageable pageable = new PageRequest(start, size, sort);
-        Page pageFromJPA = orderDAO.findAll(pageable);
-        return new Page4Navigator<>(pageFromJPA, navigatePages);
+        Pageable pageable = new PageRequest(start, size,sort);
+        Page pageFromJPA =orderDAO.findAll(pageable);
+        return new Page4Navigator<>(pageFromJPA,navigatePages);
     }
 
     public void removeOrderFromOrderItem(List<Order> orders) {
@@ -40,7 +40,7 @@ public class OrderService {
     }
 
     private void removeOrderFromOrderItem(Order order) {
-        List<OrderItem> orderItems = order.getOrderItems();
+        List<OrderItem> orderItems= order.getOrderItems();
         for (OrderItem orderItem : orderItems) {
             orderItem.setOrder(null);
         }
@@ -53,4 +53,24 @@ public class OrderService {
     public void update(Order bean) {
         orderDAO.save(bean);
     }
+
+    @Transactional(propagation= Propagation.REQUIRED,rollbackForClassName="Exception")
+    public float add(Order order, List<OrderItem> ois) {
+        float total = 0;
+        add(order);
+
+        if(false)
+            throw new RuntimeException();
+
+        for (OrderItem oi: ois) {
+            oi.setOrder(order);
+            orderItemService.update(oi);
+            total+=oi.getProduct().getPromotePrice()*oi.getNumber();
+        }
+        return total;
+    }
+    public void add(Order order) {
+        orderDAO.save(order);
+    }
+
 }

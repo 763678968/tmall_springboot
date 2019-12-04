997d1a9a3915b84aef2c1398fb19443f34a4808c
animation <763678968@qq.com>
2019-12-04 18:52:59 +0800

    前台-需要登陆-我的订单页

--- src/main/java/com/how2java/tmall/dao/OrderDAO.java
+++ src/main/java/com/how2java/tmall/dao/OrderDAO.java
@@ -1,7 +1,12 @@
 package com.how2java.tmall.dao;
 
-import com.how2java.tmall.pojo.Order;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderDAO extends JpaRepository<Order, Integer> {
+import com.how2java.tmall.pojo.Order;
+import com.how2java.tmall.pojo.User;
+
+public interface OrderDAO extends JpaRepository<Order,Integer>{
+    public List<Order> findByUserAndStatusNotOrderByIdDesc(User user, String status);
 }

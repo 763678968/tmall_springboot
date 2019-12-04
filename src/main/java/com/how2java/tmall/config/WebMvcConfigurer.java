492b0f4c9406889ed387b0f7317d8f90da5a85c0
animation <763678968@qq.com>
2019-12-04 17:01:47 +0800

    前台-需要登陆-其他拦截器

--- src/main/java/com/how2java/tmall/config/WebMvcConfigurer.java
+++ src/main/java/com/how2java/tmall/config/WebMvcConfigurer.java
@@ -1,21 +1,30 @@
 package com.how2java.tmall.config;
 
-import com.how2java.tmall.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.how2java.tmall.interceptor.LoginInterceptor;
+import com.how2java.tmall.interceptor.OtherInterceptor;
+
 @Configuration
-public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+class WebMvcConfigurer extends WebMvcConfigurerAdapter{
 
     @Bean
-    public LoginInterceptor getLoginInterceptor() {
+    public OtherInterceptor getOtherIntercepter() {
+        return new OtherInterceptor();
+    }
+    @Bean
+    public LoginInterceptor getLoginIntercepter() {
         return new LoginInterceptor();
     }
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(getLoginInterceptor()).addPathPatterns("/**");
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(getOtherIntercepter())
+                .addPathPatterns("/**");
+        registry.addInterceptor(getLoginIntercepter())
+                .addPathPatterns("/**");
     }
 }
